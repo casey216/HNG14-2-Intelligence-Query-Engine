54df850ab69a272f0050df8c57240e901e8e30a6
Kenechi Nzewi <caseynzewi@gmail.com>
2026-04-23 12:15:38 +0100

    feat: configure app behaviour for different env

--- app/core/settings.py
+++ app/core/settings.py
@@ -1,3 +1,4 @@
+import os
 from pathlib import Path
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
@@ -5,15 +6,25 @@ from pydantic_settings import BaseSettings, SettingsConfigDict
 
 BASE_DIR = Path(__file__).resolve().parent
 
+def _get_env_file() -> str:
+    if os.environ.get("TESTING") == "1":
+        return "tests/.env.test"
+    return ".env"
+
 class Settings(BaseSettings):
     """Class to hold application's config values"""
 
     model_config = SettingsConfigDict(
-        env_file=".env",
+        env_file=_get_env_file(),
         env_file_encoding="utf-8",
         extra="ignore"
     )
 
+    # Environment variables
+    ENV: str
+    TESTING: bool
+
+    # Database variables
     DB_HOST: str
     DB_PORT: int
     DB_USER: str
--- app/server.py
+++ app/server.py
@@ -2,19 +2,25 @@ from contextlib import asynccontextmanager
 
 from fastapi import FastAPI
 
+from app.core.settings import settings
 from app.db.database import init_db
 from app.models.profile import Profile
 
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    init_db()
+    if not settings.TESTING:
+        init_db()
 
     yield
 
 
 app = FastAPI(lifespan=lifespan)
 
+reload = False
+if settings.ENV == "development":
+    reload = True
+
 if __name__=="__main__":
     import uvicorn
-    uvicorn.run(app=app)
+    uvicorn.run(app="app.server:app", reload=reload)
