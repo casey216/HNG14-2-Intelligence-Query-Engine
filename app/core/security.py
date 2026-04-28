c4e3a127eff4d1e33c55dbe70145116c2acf29e9
Kenechi Nzewi <caseynzewi@gmail.com>
2026-04-28 16:41:45 +0100

    feat(auth): create verify_access_token helper function

--- app/core/security.py
+++ app/core/security.py
@@ -2,7 +2,8 @@ import hashlib
 import secrets
 from datetime import datetime, timedelta, timezone
 
-from jose import jwt
+from jose import jwt, JWTError
+from jose.exceptions import ExpiredSignatureError
 
 from app.core.settings import settings
 
@@ -16,9 +17,28 @@ def create_access_token(user_id: str) -> str:
         "exp": datetime.now(timezone.utc) + timedelta(minutes=3)
     }
 
-    return jwt.encode(payload, settings.JWT_SECRET_KEY, ALGORITHM)
+    return jwt.encode(
+        claims=payload,
+        key=settings.JWT_SECRET_KEY,
+        algorithm=ALGORITHM
+    )
 
 def create_refresh_token() -> tuple[str, str]:
     raw = secrets.token_urlsafe(32)
     hashed = hashlib.sha256(raw.encode()).hexdigest()
     return raw, hashed
+
+def verify_access_token(token: str | None) -> tuple[dict | None, str | None]:
+    if not token:
+        return None, "Token Missing"
+    
+    try:
+        return jwt.decode(
+            token=token,
+            key=settings.JWT_SECRET_KEY,
+            algorithms=[ALGORITHM],
+        ), None
+    except ExpiredSignatureError:
+        return None, "Signature Expired"
+    except JWTError:
+        return None, "Invalid Token"
